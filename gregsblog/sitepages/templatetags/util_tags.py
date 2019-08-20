f80e32da10b84b34f13d647a90ea2812de975e98
Greg Sukochev <gregsukochev@gmail.com>
2019-08-20 20:36:38 +1000

    tox

--- gregsblog/sitepages/templatetags/util_tags.py
+++ gregsblog/sitepages/templatetags/util_tags.py
@@ -8,5 +8,9 @@ register = template.Library()
 
 @register.simple_tag
 def random_url():
-    list_of_route_names = ["sitepages:view1", "sitepages:view2", "sitepages:view3"]  # names from urls.py
+    list_of_route_names = [
+        "sitepages:view1",
+        "sitepages:view2",
+        "sitepages:view3",
+    ]  # names from urls.py  # noqa: E501
     return reverse(random.choice(list_of_route_names))  # nosec
